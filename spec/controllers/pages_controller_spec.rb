d59b1b00bf6eada486ed011c897366c389c636dd
Jose Casanova <josescasanova@gmail.com>
2013-08-01 22:45:51 -0400

    Added static stuff

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
+
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.body.should include 'RoR Tutorial - Home'
+    end
+
+    it "should have a non-blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
     end
   end
 
@@ -14,6 +26,27 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'contact'
+      response.body.should include 'RoR Tutorial - Contact'
+    end
+
   end
+
+
+  describe "GET 'about'" do
+      it "returns http success" do
+        get 'about'
+        response.should be_success
+      end
+
+      it "should have the right title" do
+      get 'about'
+      response.body.should include 'RoR Tutorial - About'
+    end
+
+  end
+
 
 end
